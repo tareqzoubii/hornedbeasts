b8d0e2ba564453ec2e48d9a1a6ede100b099b901
tareqzoubii <tareqzoubi4@gmail.com>
2022-08-23 23:46:48 -0700

    V4.1

--- src/App.js
+++ src/App.js
@@ -2,26 +2,12 @@ import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Main from './Main';
-import data from './data.json';
-import SelectedBeast from './SelectedBeast';
-
 class App extends React.Component {
-constructor (props){
-  super(props)
-  this.state = {
-    ModelCards : ''
-  }
-
-  cardClickingHandler = () => {
-    alert("welcome here")
-  }
-}
-
   render(){
     return(
       <div>
         <Header/>
-        <Main data={data} handle={this.cardClickingHandler}/>       
+        <Main/>       
         <Footer/>
       </div>
     )
