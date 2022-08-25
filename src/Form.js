4a6587d97e67b4b3923245d523411449623925d0
tareqzoubii <tareqzoubi4@gmail.com>
2022-08-24 23:32:14 -0700

    V5

---
+++ src/Form.js
@@ -0,0 +1,26 @@
+import React from "react";
+// import Form from 'react-bootstrap/Form';
+
+// class Form from extends React.Component {
+//     // testClick =(val)=>{
+//     //     this.props.(val.target.value)
+//     // }
+
+//     // render() {
+//     //     return (
+//     //         <div>
+//     //         <Form className="mb-3">
+//     //     <Form.Label><h5>Choose the horn you want</h5></Form.Label>
+//     //     <Form.Select id="option" onChange ={this.testForm}>
+//     //       <option>Select Number of Horns</option>
+//     //       <option value='1'>One Horn</option>
+//     //       <option value='2'>Two Horn</option>
+//     //       <option value='3'>Three Horns</option>
+//     //       <option value='4'>One Hundred Horns</option>
+//     //     </Form.Select>
+//     //   </Form>
+//     //         </div>
+//     //     )
+//     }
+
+// export default Form;
